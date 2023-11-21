e3b475ccf8f8a51602a8a2b4f6b587598b080b63
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-11-21 18:08:05 -0300

    Add files via upload

---
+++ Cotas/index.php
@@ -0,0 +1,82 @@
+<?php
+include "connectBD.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>EEEP Manoel Mano | Cadastro Alunos</title>
+    <link rel="stylesheet" href="style-test.css">
+    <link rel="stylesheet" href="btn.css">
+    <meta name="google-site-verification" content="ezTnfxOAOeXB6FaEOsqR0fcf63u74YI3CQX8biM0w7I" />
+    <!-- Script para as Mascaras --> 
+    <script src="js/MaskInput.js"></script>
+
+    
+    <script src="js/ValidarCEP.js"></script>
+<!--     <script src="js/MaskNotas.js"></script> -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <!-- Favicon EEEP Manoel Mano -->
+    <link rel="icon" type="image/png" sizes="32x32" href="https://eeepmanoelmano.com.br/favicon.svg">
+   
+  
+</head>
+<body>
+    <div class="header">
+        <div class="container-main-menu">
+            <div class="container">
+                <div class="row-1">
+                    <div class="col-0">
+                        <a href="https://www.ceara.gov.br/"target="_blank"><img src="logo-governo.svg" alt="Logo do Governo do Ceará"></a>
+                        <a href="https://www.instagram.com/eeepmanoelmano/"target="_blank"><img src="logo-mm.svg" alt="Logo da EEEP Manoel Mano"></a>
+                    </div>
+                    <div class="col-0" id="Titulo"  >
+                        <h3>Relatório de alunos inscritos</h3>
+
+<div class="links-btn">
+        <a class="fcc-btn" href="#">Inicio</a>
+        <a class="fcc-btn" href="#">Cursos</a>
+        <a class="fcc-btn" href="#">Cadastros</a>
+        <a class="fcc-btn" href="#">Relatorios</a>
+       
+    </div>
+</div>
+
+</div>
+        </div>
+    </div>
+    </div>
+
+    
+   
+ <p class="txt">  ESCOLA ESTADUAL DE EDUCAÇÃO PROFISSIONAL MANOEL MANO 
+RESULTADO FINAL - SELEÇÃO DE ALUNOS ADESÃO 2024
+EDITAL 01/2024  </p>
+
+<p class="txt">CURSO TÉCNICO EM ENFERMAGEM
+ REDE PÚBLICA </p>
+
+
+
+
+
+<!--  <div id="current_date">
+    <script>
+        document.getElementById("current_date").innerHTML = Date();
+    </script>
+</div>
+ -->
+
+
+
+    
+        <footer><img src="./ondas-governo-rodape.png" alt=""></footer>
+        
+</body>
+</html>
