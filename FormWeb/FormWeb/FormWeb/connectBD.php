87045d9f42ed0b174be1c86ab1a4fbae843994bf
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-11-21 18:18:22 -0300

    Delete FormWeb/FormWeb directory

--- FormWeb/FormWeb/FormWeb/connectBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "trabweb";
-$username = "root";
-$password = "";
-
-$conexao = mysqli_connect($servername, $username, $password, $database)
-
-?>
--- FormWeb/FormWeb/connectBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "eeepma26_battle_royale";
-$username =  "eeepma26_edital";
-$password = "ee3pm41@3!MM";
-
-$conexao = mysqli_connect($servername, $username, $password, $database);
-
-?>
