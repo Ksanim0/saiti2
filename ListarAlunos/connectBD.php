f6612919910eaf5eab9e0d2c899a5256e586a609
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-11-21 10:51:11 -0300

    Update connectBD.php

--- ListarAlunos/connectBD.php
+++ ListarAlunos/connectBD.php
@@ -4,8 +4,8 @@
 <?php
 $servername = "localhost";
 $database = "trabweb";
-$username = "root";
-$password = "";
+$username = "eeepma26_edital";
+$password = "ee3pm41@3!MM";
 
 $conexao = mysqli_connect($servername, $username, $password, $database)
 
