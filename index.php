4901aa5e03ee6aef6b68523724ec3f0bf1cb0e0d
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-11-21 21:23:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EEEP Manoel Mano | Login</title>
+    <title>EEEP Manoel Mano | Index</title>
     <link rel="stylesheet" href="style1.css">
     <link rel="stylesheet" href="btn-style.css">
 
