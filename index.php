84ed9a544f604037c66e26b1a3f8512f9ebbd268
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-11-23 10:01:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EEEP Manoel Mano | Index</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="btn-style.css">
+    <!-- Favicon EEEP Manoel Mano -->
+    <link rel="icon" type="image/png" sizes="32x32" href="https://eeepmanoelmano.com.br/favicon.svg">
+   
+
+</head>
+<body>
+    <div class="header">
+        <div class="container-main-menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-0">
+                        <a href="https://www.ceara.gov.br/"target="_blank"><img src="./assets/logo-governo.svg" alt="Logo do Governo do Ceará"></a>
+                        <a href="https://eeepmanoelmano.com.br/" target="_self"><img src="./assets/logo-mm.svg" alt="Logo da EEEP Manoel Mano" ></a>
+                    </div>
+                    <div class="col-0">
+                        <h3>Tela de Inicio</h3>
+
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="area-formulario">
+            <div class="formulario">
+                    <div class="botoes">
+                        <a href="./FormWeb-IN/index.php">  <button class="btn" target ="_blank"> <img src="./assets/icon.svg"> <br> CADASTRO DE ALUNOS</button></a>
+                       <a href="./alunosInscritos/index.php"> <button class="btn" id="sla"> <img src="./assets/icon2.svg" alt="" height="139px"> <br> GERAR RELATÓRIO</button></a>
+                        <a href="./ListarAlunos/index.php"><button class="btn"> <img src="./assets/icon3.svg" alt=""> <br>  ALUNOS CADASTRADOS</button></a>
+                     </div>
+                    </div>
+              
+            </div>
+        </div>
+    </div>
+        <footer><img src="./assets/ondas-governo-rodape.png" alt=""></footer>
+</body>
+</html>
