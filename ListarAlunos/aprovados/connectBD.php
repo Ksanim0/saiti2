e90453f147f8048c1998063e9a04127dc15889b5
Edmilson Siqueira Braz Junior <100867319+Ksanim0@users.noreply.github.com>
2023-11-23 11:12:35 -0300

    Add files via upload

--- ListarAlunos/aprovados/connectBD.php
+++ ListarAlunos/aprovados/connectBD.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$database = "trabweb"; #eeepma26_battle_royale
-$username = "root";
-$password = "";
+$database = "eeepma26_battle_royale"; #eeepma26_battle_royale
+$username = "root";#eeepma26_edital
+$password = "";#ee3pm41@3!MM
 
 $conexao = mysqli_connect($servername, $username, $password, $database)
 
